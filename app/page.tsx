0cc8b5867874a50c3ab91a459823d92cf1dd5b3e
lematrixai <erickbale360@gmail.com>
2025-07-31 19:54:49 +0300

    patterns

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import HeroSection from '@/components/hero-section'
 import Header from '@/components/header'
+import Tours from '@/components/tours'
 
 const LandingPage = () => {
   return (
     <div>
       <Header />
       <HeroSection />
+      <Tours />
     </div>
   )
 }
